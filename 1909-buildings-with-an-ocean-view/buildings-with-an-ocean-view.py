ea62a014002db0825049a102a3d079b772887393
thilak15 <thilakreddy125@gmail.com>
2024-01-28 15:40:45 -0500

    Time: 531 ms (58.40%) | Memory: 33.9 MB (83.85%) - LeetSync

---
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findBuildings(self, heights: List[int]) -> List[int]:
+        """res = []
+        max_height = 0
+        n = len(heights)
+        for i in range(n-1, -1,-1):
+            if heights[i] > max_height:
+                res.append(i)
+                max_height = heights[i]
+        return res[::-1]"""
+
+    # The question gave me the hint to traverse from right to left than left to right
+    # Time complexity O(n)
+    # space complexit O(n)
+
+    # now using Stack
+        stack = []
+        n = len(heights)
+        for i in range(n):
+            while stack and heights[i] >= heights[stack[-1]]:
+                stack.pop()
+            stack.append(i)
+        return stack
+    # Time complexity O(n)
+    # space Complexity O(n)
