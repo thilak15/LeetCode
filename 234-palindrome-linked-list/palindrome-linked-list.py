f5836478314b76a73530b530f34a7ed911a713ec
thilak15 <thilakreddy125@gmail.com>
2024-02-01 14:25:58 -0500

    Time: 278 ms (94.19%) | Memory: 32 MB (89.47%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -6,7 +6,7 @@
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         
-        def reverseLinkedList(orginalHead):
+        """def reverseLinkedList(orginalHead):
             prev,curr=None,orginalHead
 
             while curr:
@@ -25,4 +25,25 @@ class Solution:
                 return False
             curr1=curr1.next
             curr2=curr2.next
+        return True"""
+    # Time complexit is O(n)
+    # Space Complexity O(n)
+    # we can reduce the space complexit by just reversing the second half of the Linked list using two pointer concept(slow and fast)
+        slow=fast=head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+        prev=None
+        while slow:
+            temp=slow.next
+            slow.next=prev
+            prev=slow
+            slow=temp
+        left,right=head,prev
+        while right:
+            if left.val!=right.val:
+                return False
+            left=left.next
+            right=right.next
         return True
+
