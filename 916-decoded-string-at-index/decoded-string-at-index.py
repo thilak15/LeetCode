ae69062f7fb6fc37bf1adb5c28cf2e79dc70fa34
thilak15 <thilakreddy125@gmail.com>
2024-01-31 20:05:19 -0500

    Time: 27 ms (96.66%) | Memory: 16.6 MB (69.55%) - LeetSync

--- 916-decoded-string-at-index/decoded-string-at-index.py
+++ 916-decoded-string-at-index/decoded-string-at-index.py
@@ -14,7 +14,6 @@ class Solution:
                 size//=int(c)
             else:
                 size-=1
-        print(k)
 # We dont have to form the string we can just keep track of the size o f the string and the traverse from back and retun hte char when k is 0
 # Time Complexity O(n)
 # Space Complexit O(1)
